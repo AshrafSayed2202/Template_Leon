fd0180d93b58d56986a5300418b09d6d5d4a3acc
Ashraf Sayed <ashraf.neizk@gmail.com>
2022-04-20 20:03:32 +0200

    Upload

---
+++ JS/main.js
@@ -0,0 +1,12 @@
+let showMenu = document.getElementById("menu_icon");
+let menu = document.querySelector(".menu-content")
+let middleSpan = document.querySelector(".menu-icon span:nth-child(2)");
+showMenu.onclick = function(){
+    menu.style.display = "flex";
+    middleSpan.style.width = "100%"
+};
+let xMark = document.getElementById("close_menu");
+xMark.onclick = function(){
+    menu.style.display = "none";
+    middleSpan.style.width = "66.6%"
+}
